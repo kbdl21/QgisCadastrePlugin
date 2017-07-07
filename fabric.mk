40a996f1702680a68c13429b8d32fefc118187ed
Laurent Jouanneau <dev@ljouanneau.com>
2017-07-07 17:48:21 +0200

    Makefile to build on 3liz infra

---
+++ fabric.mk
@@ -0,0 +1,28 @@
+#
+# Makefile for building/packaging qgis for lizmap hosting
+#
+
+ifndef FABRIC
+FABRIC:=$(shell [ -e .fabricrc ] && echo "fab -c .fabricrc" || echo "fab")
+endif
+
+VERSION=$(shell python metadata.py metadata.txt version)
+
+main:
+	echo "Makefile for packaging infra components: select a task"
+
+PACKAGE=qgis_cadastre
+FILES = composers filters forms icons interface scripts styles templates *.py *.qrc icon.png metadata.txt README.md LICENSE
+
+
+build/cadastre:
+	@rm -rf build/cadastre
+	@mkdir -p build/cadastre
+
+
+.PHONY: package
+package: build/cadastre
+	@echo "Building package qgis_cadastre"
+	@cp -rLp $(FILES) build/cadastre/
+	$(FABRIC) -f $(LIZFABFILE) package:qgis_cadastre,versiontag=$(VERSION),files=cadastre,directory=./build
+
